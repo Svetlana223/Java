45fb66d7028a4ec1200f13364cc6f22a2b22af46
Svetlana <strel_sv@bk.ru>
2023-03-30 17:18:30 -0400

    add change homework6

--- src/main/java/org/example/sem6/homework/model/Filter.java
+++ src/main/java/org/example/sem6/homework/model/Filter.java
@@ -2,34 +2,56 @@ package org.example.sem6.homework.model;
 
 import java.util.*;
 
-public class Filter {
-    public static void filter(Set laptopsSet){
-        HashSet<Laptop> laptops = new HashSet<>(laptopsSet);
-        Map<Integer,String> map = new HashMap<>();
-        map.put(1, "ram");
-        map.put(2, "rom");
-        map.put(3,"operatingSystem");
-        map.put(4, "color");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("“Введите цифру, соответствующую необходимому критерию:\n" +
-                "1 - ОЗУ\n" +
-                "2 - Объем ЖД\n" +
-                "3 - Операционная система\n" +
-                "4 - Цвет\n");
-        int n = scanner.nextInt();
-        for (Laptop el: laptops) {
-            System.out.println(el);
-        }
-        if (n == 1) {
-            System.out.println("Введите необходимую память: " );
-            int memory = scanner.nextInt();
-            if (laptops.equals(memory)){
-                System.out.println(laptops.equals(memory));
-            }else System.out.println("Нет ноутбуков с данной памятью");
+import lombok.Data;
+
+@Data
+
+public class Filter extends Laptop {
+    public Filter(String name, String color, String operatingSystem, Integer rom, Integer ram) {
+        super(name, color, operatingSystem, rom, ram);
+    }
+
+    public void filter(Set laptopsSet) {
+        HashSet<Laptop> laptops = new HashSet<>(laptopsSet);
+        System.out.print("Введите цифру, соответствующую необходимому критерию: \n " +
+                "1 - Название: \n" +
+                "2 - ОЗУ: \n" +
+                "3 - Объем ЖД: \n" +
+                "4 - Операционная система \n" +
+                "5 - Цвет");
+        Scanner sc = new Scanner(System.in);
+        int choise = sc.nextInt();
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "name");
+        map.put(2, "ram");
+        map.put(3, "rom");
+        map.put(4, "operatingSystem");
+        map.put(5, "color");
+
+
+        switch (choise) {
+            case 1 -> this.setName(askString());
+            case 2 -> this.setRam(askInt());
+            case 3 -> this.setRom(askInt());
+            case 4 -> this.setOperatingSystem(askString());
+            case 5 -> this.setColor(askString());
         }
 
 
     }
+
+
+    private Integer askInt() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    private static String askString() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
 
 }
